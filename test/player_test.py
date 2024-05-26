2134e958e479854d7b99175ad23f07d25d546d05
boris yelnats <52521179+borisyelnats@users.noreply.github.com>
2024-05-26 20:14:41 +0800

    changes made

--- test/player_test.py
+++ test/player_test.py
@@ -1,8 +1,9 @@
 import unittest
 import sys
-sys.path.append("/Users/benjamincooper/Documents/TAFE/advanced programming/AP_ass1/SRUS-BJC-Games/app")
-from player import player 
+sys.path.append("/Users/benjamincooper/Documents/TAFE/advanced programming/AP_ass1/SRUS-BJC-Games/")
+from app.player import player 
 import argon2
+
 
 #to run: python -m unittest player_test.py
 
@@ -42,6 +43,20 @@ class player_test(unittest.TestCase):
         person1.set_score(4)
         person2.set_score(4)
         self.assertTrue(person1 == person2)
+
+    def test_sort(self):
+        player1 = player(1,"Ben")
+        player1.set_score(3)
+        player2 = player(2,"Ben")
+        player2.set_score(2)
+        player3 = player(3,"Ben")
+        player3.set_score(1)
+        player4 = player(4,"Ben")
+        player4.set_score(5)
+        player.sort_all_instances()
+        for i in range(len(player.player_instances)-1):
+            self.assertTrue(player.player_instances[i] >= player.player_instances[i+1])
+
 
 
 
